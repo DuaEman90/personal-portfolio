bf3ea7135614fcf01f0f6307fbde7db05bf044ed
Dua Eman <d27065204@gmail.com>
2026-09-01 12:41:03 +0500

    feat: add Skills component to App for improved portfolio showcase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About"; 
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <About/>
-
+    <Skills/>
       {/* Temporary sections */}
       <section id="about" className="min-h-screen bg-[#080d18]" />
       <section id="skills" className="min-h-screen bg-[#040813]" />
