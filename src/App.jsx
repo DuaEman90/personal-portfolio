86539e4678137a6fe6ee47f2c440c1877a8b532c
Dua Eman <d27065204@gmail.com>
2026-09-02 19:52:00 +0500

    feat: add Skills component back to App for enhanced layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About"; 
-
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <About/>
+      <Skills />
    
       {/* Temporary sections */}
       <section id="about" className="min-h-screen bg-[#080d18]" />
